6ed34c7ca7c30f41176bc1531018896e47aa5d05
Kevin Schneider <schneider.kev@outlook.de>
2021-01-08 11:02:44 +0100

    Add customization options , incude in docs/readme

--- Content/docs/index.fsx
+++ Content/docs/index.fsx
@@ -4,12 +4,26 @@
 This template scaffolds the necessary folder structure for FSharp.Formatting 
 and adds custom styles in the fslab theme. 
 
+The provided stylesheet was compiled from sass (befor uploading the nuget package) and
+uses the [Bulma](https://bulma.io/) CSS framework instead of bootstrap which is used by FSharp.Formatting per default.
+
+#### Table of contents 
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Quick content rundown](#Quick-content-rundown)
+- [Creating new content](#Creating-new-content)
+- [Customization options](#Customization-options)
+    - [Style sheet options](#Style-sheet-options)
+    - [Inclusion of sample content](#Inclusion-of-sample-content)
+
+
 ## Installation
 
-This template is available as a dotnet new template:
+This template is available as a dotnet new template (from [nuget](https://www.nuget.org/packages/FsLab.DocumentationTemplate/)):
 
 ```no-highlight
-dotnet new -i FsLab.DocumentationTemplate - 
+dotnet new -i FsLab.DocumentationTemplate
 ```
 
 ## Usage
@@ -19,6 +33,9 @@ in the root of your project that you want to write documentation for, run:
 ```no-highlight
 dotnet new fslab-docs
 ```
+
+See [further below](#Customization-options) for command line customization options of the template:
+
 
 ## Quick content rundown:
 
@@ -46,7 +63,7 @@ docs
 - `index.fsx` is the file you are reading just now. It contains the very content you are reading at the moment 
 in a markdown block indicated by `(** *)` guards. It will be rendered as the root `index.html` file of your documentation.
 
-- `_template.html` is the root html scaffold (sidebar to the left, script and style loading)
+- `_template.html` is the root html scaffold (sidebar to the left, script and style loading) where all of the individual docs will be injected into
 
 - the `content` folder contains the following files:
 
@@ -64,6 +81,50 @@ in a markdown block indicated by `(** *)` guards. It will be rendered as the roo
 
 ## Creating new content
 
-Please refer to the [FSharp.Formatting documentation]().
+- run `dotnet fsdocs watch --eval` to spawn a watcher and dev server that hosts your docs on http://localhost:8901/ (You currently will still have to refresh the page when you make changes to files)
 
+- add a new .md or .fsx file to the `content` directory (or into a new subdirectory there)
+
+- the sidebar title for the document will be either the file name or, if existent, the first level 1 header in the file
+
+- when writing a .fsx file, code will automatically become syntax-highlighted code snippets. 
+
+- use `(** <markdown here> *)` to guard markdown sections in .fsx files
+
+- use `(*** include-value:<val name> ***)` to include the value of a binding
+
+- use `(*** include-it ***)` to include the evaluation of the previous snippet block 
+
+For more info please refer to the [FSharp.Formatting documentation](http://fsprojects.github.io/FSharp.Formatting/).
+
+
+## Customization options
+
+### Style sheet options
+
+
+
+```no-highlight
+-s|--styles             Set the type of style content the template will initialize. For the sass file to work, you will have to download bulma
+
+        all             - sass file, compiled csss, and minified css
+
+        sass            - only include the sass file
+
+        minified        - only include the minified css file
+
+        css             - only include the compiled css file
+
+        Default:        css
+```
+
+### Inclusion of sample content
+
+```no-highlight
+-is|--include-samples   wether to include sample files in the generated content
+
+        bool            - Optional
+
+        Default:        true
+```
 *)
--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+## Version 0.1.1 - 1/5/2020
+* Add some cli customization options to the template
+
 ## Version 0.1 - 1/5/2020
 * Add stylesheet compiled from sass with imported bulma
 * Adapt html templates to bulma instead of bootstrap
