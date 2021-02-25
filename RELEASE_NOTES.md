f4273cbbde74bf81305ef56542e2ed9813ca0431
Kevin Schneider <schneider.kev@outlook.de>
2021-02-25 13:33:44 +0100

    Update build script and release notes

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+## Version 1.1.1 - 2/25/2021
+* Update fsdocs version to 9.0.1
+* Lock fsdocs version
+
 ## Version 1.1.0 - 1/28/2021
 * [Add ipynb docs and cli switch](https://github.com/fslaborg/docs-template/commit/5eb1c359ab2958bdd3cf1efbd550a1380801d4c0)
 * [Move sample content and fix cli switch](https://github.com/fslaborg/docs-template/commit/25dc4f5b4ca4dfcffa9e0fb26c010b1c496db718)
--- build.fsx
+++ build.fsx
@@ -32,97 +32,268 @@ open Fake.Tools.Git
 
 Target.initEnvironment ()
 
-let release = ReleaseNotes.load ("RELEASE_NOTES.md")
+[<AutoOpen>]
+/// user interaction prompts for critical build tasks where you may want to interrupt when you see wrong inputs.
+module MessagePrompts =
 
-let version = SemVer.parse release.NugetVersion
+    let prompt (msg:string) =
+        System.Console.Write(msg)
+        System.Console.ReadLine().Trim()
+        |> function | "" -> None | s -> Some s
+        |> Option.map (fun s -> s.Replace ("\"","\\\""))
 
+    let rec promptYesNo msg =
+        match prompt (sprintf "%s [Yn]: " msg) with
+        | Some "Y" | Some "y" -> true
+        | Some "N" | Some "n" -> false
+        | _ -> System.Console.WriteLine("Sorry, invalid answer"); promptYesNo msg
+
+    let releaseMsg = """This will stage all uncommitted changes, push them to the origin and bump the release version to the latest number in the RELEASE_NOTES.md file. 
+        Do you want to continue?"""
+
+    let releaseDocsMsg = """This will push the docs to gh-pages. Remember building the docs prior to this. Do you want to continue?"""
+
+/// Executes a dotnet command in the given working directory
 let runDotNet cmd workingDir =
     let result =
         DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
     if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
 
-//let withProjectRoot dir = __SOURCE_DIRECTORY__ @@ dir
+/// Metadata about the project
+module ProjectInfo = 
 
-let clean = BuildTask.create "clean" [] {
-    [
-        "bin"
-        "obj"
-        "pkg"
-        "tests"
-        "Content/bin"
-        "Content/obj"
-        "Content/.fsdocs"
-        "Content/tmp"
-    ]
-    |> Shell.cleanDirs
+    let project = "docs-template"
 
-    [
-        "Content/docs/content/fsdocs-custom.min.css.gz"
-        "Content/docs/content/fsdocs-custom.min.css"
-        "Content/docs/content/fsdocs-custom.css"
-    ]
-    |> File.deleteAll
-}
+    let summary = "fslab documentation theme for FSharp.Formatting"
 
-let compileSass = BuildTask.create "compileSass" [clean] {
-    !! "Content/docs/**/*.scss"
-    ++ "Content/docs/*.scss"
-    |> Seq.iter (fun sassFile -> 
-        let targetFile = sassFile.Replace(".scss", ".css")
-        let configFile = (__SOURCE_DIRECTORY__ + "/webcompilerconfiguration.json")
-        let cmd = sprintf "webcompiler %s %s -c %s" sassFile targetFile configFile
-        printfn "[Sass]: Compiling %s --> %s" sassFile targetFile
-        runDotNet cmd (Path.getDirectory sassFile)
-    )
-}
+    let configuration = "Release"
 
-let buildTestProject = BuildTask.create "buildTestProject" [clean] { 
-    !! "Content/*.fsproj"
-    |> Seq.iter (DotNet.build id)
-}
+    // Git configuration (used for publishing documentation in gh-pages branch)
+    // The profile where the project is posted
+    let gitOwner = "fslaborg"
+    let gitName = "docs-template"
 
-let watchExampleDocs = BuildTask.create "watchExampleDocs" [clean; compileSass; buildTestProject] { 
-    runDotNet "fsdocs watch --eval --strict --clean --property Configuration=Release" "Content"
-}
+    let gitHome = sprintf "%s/%s" "https://github.com" gitOwner
 
-let buildExampleDocs = BuildTask.create "buildExampleDocs" [clean; compileSass; buildTestProject] { 
-    runDotNet "fsdocs build --eval --strict --clean --property Configuration=Release --output ../gh-pages" "Content"
-}
+    let projectRepo = sprintf "%s/%s/%s" "https://github.com" gitOwner gitName
 
-let pack = BuildTask.create "Pack" [clean; compileSass;] {
-    !! "./FsLab.DocumentationTemplate.fsproj"
-    |> Seq.iter (Fake.DotNet.DotNet.pack (fun p ->
-        let msBuildParams =
-            {p.MSBuildParams with 
-                Properties = ([
-                    "Version",(sprintf "%i.%i.%i" version.Major version.Minor version.Patch )
-                    "PackageReleaseNotes",  (release.Notes |> String.concat "\r\n")
-                ] @ p.MSBuildParams.Properties
-                )
+    let website = "/docs-template"
+
+    let pkgDir = "pkg"
+
+    let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+    let stableVersion = SemVer.parse release.NugetVersion
+
+    let stableVersionTag = (sprintf "%i.%i.%i" stableVersion.Major stableVersion.Minor stableVersion.Patch )
+
+    let mutable prereleaseSuffix = ""
+
+    let mutable prereleaseTag = ""
+
+    let mutable isPrerelease = false
+
+/// Barebones, minimal build tasks
+module BasicTasks = 
+
+    open ProjectInfo
+
+    let setPrereleaseTag = BuildTask.create "SetPrereleaseTag" [] {
+        printfn "Please enter pre-release package suffix"
+        let suffix = System.Console.ReadLine()
+        prereleaseSuffix <- suffix
+        prereleaseTag <- (sprintf "%s-%s" release.NugetVersion suffix)
+        isPrerelease <- true
+    }
+
+    let clean = BuildTask.create "clean" [] {
+        [
+            "bin"
+            "obj"
+            "pkg"
+            "tests"
+            "Content/bin"
+            "Content/obj"
+            "Content/.fsdocs"
+            "Content/tmp"
+        ]
+        |> Shell.cleanDirs
+
+        [
+            "Content/docs/content/fsdocs-custom.min.css.gz"
+            "Content/docs/content/fsdocs-custom.min.css"
+            "Content/docs/content/fsdocs-custom.css"
+        ]
+        |> File.deleteAll
+    }
+
+    let compileSass = BuildTask.create "compileSass" [clean] {
+        !! "Content/docs/**/*.scss"
+        ++ "Content/docs/*.scss"
+        |> Seq.iter (fun sassFile -> 
+            let targetFile = sassFile.Replace(".scss", ".css")
+            let configFile = (__SOURCE_DIRECTORY__ + "/webcompilerconfiguration.json")
+            let cmd = sprintf "webcompiler %s %s -c %s" sassFile targetFile configFile
+            printfn "[Sass]: Compiling %s --> %s" sassFile targetFile
+            runDotNet cmd (Path.getDirectory sassFile)
+        )
+    }
+
+    let buildTestProject = BuildTask.create "buildTestProject" [clean] { 
+        !! "Content/*.fsproj"
+        |> Seq.iter (DotNet.build id)
+    }
+
+/// Package creation
+module PackageTasks = 
+
+    open ProjectInfo
+    open BasicTasks
+
+    let pack = BuildTask.create "Pack" [clean; compileSass;] {
+        !! "./FsLab.DocumentationTemplate.fsproj"
+        |> Seq.iter (Fake.DotNet.DotNet.pack (fun p ->
+            let msBuildParams =
+                {p.MSBuildParams with 
+                    Properties = ([
+                        "Version",stableVersionTag
+                        "PackageReleaseNotes",  (release.Notes |> String.concat "\r\n")
+                    ] @ p.MSBuildParams.Properties
+                    )
+                }
+            {
+                p with 
+                    MSBuildParams = msBuildParams
+                    OutputPath = Some "pkg"
             }
-        {
-            p with 
-                MSBuildParams = msBuildParams
-                OutputPath = Some "pkg"
-        }
-    ))
-}
+        ))
+    }
+    
+    let packPrerelease = BuildTask.create "PackPrerelease" [setPrereleaseTag; clean; compileSass;] {
+        !! "./FsLab.DocumentationTemplate.fsproj"
+        |> Seq.iter (Fake.DotNet.DotNet.pack (fun p ->
+            let msBuildParams =
+                {p.MSBuildParams with 
+                    Properties = ([
+                        "Version",prereleaseTag
+                        "PackageReleaseNotes",  (release.Notes |> String.concat "\r\n")
+                    ] @ p.MSBuildParams.Properties
+                    )
+                }
+            {
+                p with 
+                    MSBuildParams = msBuildParams
+                    OutputPath = Some "pkg"
+            }
+        ))
+    }
 
-let uninstallTestTemplate =  BuildTask.create "uninstallTestTemplate" [] {
-    runDotNet "new -u FsLab.DocumentationTemplate" __SOURCE_DIRECTORY__
-}
+module TestTasks = 
+    
+    open ProjectInfo
+    open BasicTasks
+    open PackageTasks
 
-let installTestTemplate =  BuildTask.create "installTestTemplate" [pack] {
-    runDotNet 
-        (sprintf "new -i FsLab.DocumentationTemplate.%i.%i.%i.nupkg" version.Major version.Minor version.Patch)
-        (__SOURCE_DIRECTORY__ @@ "pkg")
-}
+    let uninstallTestTemplate =  BuildTask.create "uninstallTestTemplate" [] {
+        runDotNet "new -u FsLab.DocumentationTemplate" __SOURCE_DIRECTORY__
+    }
 
-let runTestTemplate =  BuildTask.create "runTestTemplate" [installTestTemplate] {
-    Directory.ensure (__SOURCE_DIRECTORY__ @@ "tests")
-    runDotNet "new fslab-docs" (__SOURCE_DIRECTORY__ @@ "tests")
-}
+    let installTestTemplate =  BuildTask.create "installTestTemplate" [pack] {
+        runDotNet 
+            (sprintf "new -i FsLab.DocumentationTemplate.%s.nupkg" (if isPrerelease then prereleaseTag else stableVersionTag))
+            (__SOURCE_DIRECTORY__ @@ "pkg")
+    }
 
-let test = BuildTask.createEmpty "test" [installTestTemplate; runTestTemplate; uninstallTestTemplate]
+    let runTestTemplate =  BuildTask.create "runTestTemplate" [installTestTemplate] {
+        Directory.ensure (__SOURCE_DIRECTORY__ @@ "tests")
+        runDotNet "new fslab-docs" (__SOURCE_DIRECTORY__ @@ "tests")
+    }
+
+    let test = BuildTask.createEmpty "test" [installTestTemplate; runTestTemplate; uninstallTestTemplate]
+
+module ReleaseTasks =
+
+    open ProjectInfo
+
+    open BasicTasks
+    open PackageTasks
+
+    let createTag = BuildTask.create "CreateTag" [clean; compileSass; pack] {
+        if promptYesNo (sprintf "tagging branch with %s OK?" stableVersionTag ) then
+            Git.Branches.tag "" stableVersionTag
+            Git.Branches.pushTag "" projectRepo stableVersionTag
+        else
+            failwith "aborted"
+    }
+
+    let createPrereleaseTag = BuildTask.create "CreatePrereleaseTag" [setPrereleaseTag; clean; compileSass; pack] {
+        if promptYesNo (sprintf "tagging branch with %s OK?" prereleaseTag ) then 
+            Git.Branches.tag "" prereleaseTag
+            Git.Branches.pushTag "" projectRepo prereleaseTag
+        else
+            failwith "aborted"
+    }
+
+    
+    let publishNuget = BuildTask.create "PublishNuget" [clean; compileSass; pack] {
+        let targets = (!! (sprintf "%s/*.*pkg" pkgDir ))
+        for target in targets do printfn "%A" target
+        let msg = sprintf "release package with version %s?" stableVersionTag
+        if promptYesNo msg then
+            let source = "https://api.nuget.org/v3/index.json"
+            let apikey =  Environment.environVar "NUGET_KEY"
+            for artifact in targets do
+                let result = DotNet.exec id "nuget" (sprintf "push -s %s -k %s %s --skip-duplicate" source apikey artifact)
+                if not result.OK then failwith "failed to push packages"
+        else failwith "aborted"
+    }
+
+    let publishNugetPrerelease = BuildTask.create "PublishNugetPrerelease" [setPrereleaseTag; clean; compileSass; pack] {
+        let targets = (!! (sprintf "%s/*.*pkg" pkgDir ))
+        for target in targets do printfn "%A" target
+        let msg = sprintf "release package with version %s?" prereleaseTag 
+        if promptYesNo msg then
+            let source = "https://api.nuget.org/v3/index.json"
+            let apikey =  Environment.environVar "NUGET_KEY"
+            for artifact in targets do
+                let result = DotNet.exec id "nuget" (sprintf "push -s %s -k %s %s --skip-duplicate" source apikey artifact)
+                if not result.OK then failwith "failed to push packages"
+        else failwith "aborted"
+    }
+
+/// Build tasks for documentation setup and development
+module DocumentationTasks =
+
+    open ProjectInfo
+
+    open BasicTasks
+
+    let buildDocs = BuildTask.create "BuildDocs" [clean; compileSass; buildTestProject] {
+        printfn "building docs with stable version %s" stableVersionTag
+        runDotNet "fsdocs build --eval --strict --clean --properties Configuration=Release --output ../gh-pages" "Content"
+    }
+
+    let buildDocsPrerelease = BuildTask.create "BuildDocsPrerelease" [setPrereleaseTag; clean; compileSass; buildTestProject] {
+        printfn "building docs with prerelease version %s" prereleaseTag
+        runDotNet 
+            (sprintf "fsdocs build --eval --strict --clean --properties Configuration=Release --output ../gh-pages --parameters fsdocs-package-version %s" prereleaseTag)
+            "./"
+    }
+
+    let watchDocs = BuildTask.create "WatchDocs" [clean; compileSass; buildTestProject] {
+        printfn "watching docs with stable version %s" stableVersionTag
+        runDotNet "fsdocs watch --eval --strict --clean --properties Configuration=Release" "Content"
+    }
+
+    let watchDocsPrerelease = BuildTask.create "WatchDocsPrerelease" [setPrereleaseTag; clean; compileSass; buildTestProject] {
+        printfn "watching docs with prerelease version %s" prereleaseTag
+        runDotNet (sprintf "fsdocs watch --eval --strict --clean --properties Configuration=Release --parameters %s" prereleaseTag) "Content"
+    }
+
+open ProjectInfo
+open BasicTasks
+open PackageTasks
+open TestTasks
+open ReleaseTasks
 
 BuildTask.runOrDefaultWithArguments pack
